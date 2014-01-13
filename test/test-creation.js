12317c470f6229112f977a39fac7fa011f399122
labrador <labrador@labradoodle.com>
2014-01-13 16:45:03 -0700

    updated tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -5,29 +5,76 @@ var path    = require('path');
 var helpers = require('yeoman-generator').test;
 
 
-describe('protractor-pageobjects generator', function () {
+describe('ptor generator', function () {
     beforeEach(function (done) {
         helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
             if (err) {
                 return done(err);
             }
 
-            this.app = helpers.createGenerator('protractor-pageobjects:app', [
+            this.app = helpers.createGenerator('ptor:app', [
                 '../../app'
             ]);
             done();
         }.bind(this));
     });
 
-    it('creates expected files', function (done) {
+    it('creates expected files with coffeescript opted in', function (done) {
         var expected = [
-            // add files you expect to exist here.
             '.jshintrc',
-            '.editorconfig'
+            '.editorconfig',
+            'package.json',
+            'bower.json',
+            'Gruntfile.js',
+            'protractor.conf.js',
+            'protractor-sauce.conf.js',
+            'README.md',
+            'bin/element_explorer.sh',
+            'bin/install.sh',
+            'bin/selenium-server.sh',
+            'bin/test.sh',
+            'bin/test-sauce.sh',
+            'lib/pageObject.js',
+            'pageObjects/builtWithAngular.coffee',
+            'specs/builtWithAngularSpec.coffee'
+
+
         ];
 
         helpers.mockPrompt(this.app, {
-            'someOption': true
+            'coffee': true
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+            helpers.assertFiles(expected);
+            done();
+        });
+    });
+
+    it('creates expected files with javascript opted in', function (done) {
+        var expected = [
+            '.jshintrc',
+            '.editorconfig',
+            'package.json',
+            'bower.json',
+            'Gruntfile.js',
+            'protractor.conf.js',
+            'protractor-sauce.conf.js',
+            'README.md',
+            'bin/element_explorer.sh',
+            'bin/install.sh',
+            'bin/selenium-server.sh',
+            'bin/test.sh',
+            'bin/test-sauce.sh',
+            'lib/pageObject.js',
+            'pageObjects/builtWithAngular.js',
+            'specs/builtWithAngularSpec.js'
+
+
+        ];
+
+        helpers.mockPrompt(this.app, {
+            'coffee': false
         });
         this.app.options['skip-install'] = true;
         this.app.run({}, function () {
--- test/test-load.js
+++ test/test-load.js
@@ -3,7 +3,7 @@
 
 var assert = require('assert');
 
-describe('protractor-pageobjects generator', function () {
+describe('ptor generator', function () {
     it('can be imported without blowing up', function () {
         var app = require('../app');
         assert(app !== undefined);
